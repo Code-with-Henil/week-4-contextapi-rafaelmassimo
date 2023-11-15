d968f415476fdfa59cb7629f52c5a828bdeac68d
Rafael Massimo <rafael_massimo@hotmail.com>
2023-11-15 15:54:56 -0800

    improve by adding usestate

--- ContextAPI/src/components/ConvertCurrency.jsx
+++ ContextAPI/src/components/ConvertCurrency.jsx
@@ -5,8 +5,10 @@ import { CurrencyContext } from "../contexts/CurrencyContext";
 
 function ConvertCurrency() {
 	const { currentCurrency, amount, colorTheme } = useContext(CurrencyContext);
-	const { setCurrentCurrency, setAmount, setColorTheme } =
-		useContext(CurrencyContext);
+	const { setCurrentCurrency, setAmount, setColorTheme } = useContext(CurrencyContext);
+
+	const [updatedCurrency, setUpdatedCurrency] = useState("");
+	const [updatedAmount, setUpdatedAmount] = useState(0);
 
 	const handleCurrencyChange = (e) => {
 		e.preventDefault();
@@ -16,30 +18,27 @@ function ConvertCurrency() {
 		setCurrentCurrency(selectedCurrency);
 	};
 
-	let currentCurrencyInput;
-	let currentAmount;
 	const usDolarValue = 4.87;
 	const cadDolarValue = 3.6;
 
 	useEffect(() => {
-		currentCurrencyInput = currentCurrency;
-		currentAmount = amount;
+		setUpdatedCurrency(currentCurrency);
+		setUpdatedAmount(amount);
 	}, [currentCurrency, amount]);
 
 	const [amoutConverted, setAmoutConverted] = useState(0);
 
 	function convertAmount() {
-        if(currentAmount === 0) {
-            alert("Please input a value to be converted")
-        }
-
-		if (currentCurrencyInput === "US-dolar") {
-			setAmoutConverted(currentAmount / usDolarValue);
-		} else if (currentCurrencyInput === "CAD-dolar") {
-			setAmoutConverted(currentAmount / cadDolarValue);
+		if (updatedAmount === 0) {
+			alert("Please input a value to be converted");
+		}
+		if (updatedCurrency === "US-dolar") {
+			setAmoutConverted(updatedAmount / usDolarValue);
+		} else if (updatedCurrency === "CAD-dolar") {
+			setAmoutConverted(updatedAmount / cadDolarValue);
 		} else {
-            alert("You need to choose a currency option")
-        }
+			alert("You need to choose a currency option");
+		}
 	}
 
 	return (
@@ -59,21 +58,24 @@ function ConvertCurrency() {
 				</button>
 			</nav>
 			<div className="p-48 transition duration-1000 ease-in-out flex flex-col w-3/5">
-				<form className="flex flex-col" action={convertAmount}>
+				<form className="flex flex-col" onSubmit={convertAmount}>
 					<p className="text-3xl font-bold font-normal">
-						From Reais Brazil:{' '}{amount.toLocaleString(undefined, {
-                            style: "currency",
-                            currency: "BRL"
-                        })}
+						From Reais Brazil:{" "}
+						{amount.toLocaleString(undefined, {
+							style: "currency",
+							currency: "BRL",
+						})}
 					</p>
 					<br />
-					{currentCurrency !== "" && <h2 className="text-3xl font-semibold m-4">
-						To {currentCurrency}:{' '}
-						{amoutConverted.toLocaleString(undefined, {
-							style: "currency",
-							currency: "USD",
-						})}
-					</h2>}
+					{currentCurrency !== "" && (
+						<h2 className="text-3xl font-semibold m-4">
+							To {currentCurrency}:{" "}
+							{amoutConverted.toLocaleString(undefined, {
+								style: "currency",
+								currency: "USD",
+							})}
+						</h2>
+					)}
 					<label htmlFor="currency">
 						Select The Currency to be convert in:{" "}
 					</label>
@@ -90,19 +92,21 @@ function ConvertCurrency() {
 						<option value="CAD-dolar">CAD Dolar</option>
 					</select>
 
-					<label htmlFor="amount">Insert here the amount in R$ to be converted</label>
+					<label htmlFor="amount">
+						Insert here the amount in R$ to be converted
+					</label>
 					<input
 						className={`rounded-md m-3 p-2 transition duration-500 ease-in-out ${
 							colorTheme === "light" ? "text-black " : "bg-cyan-950 text-white"
 						}`}
 						onChange={(e) => {
-                            const inputValue = e.target.value;
+							const inputValue = e.target.value;
 							setAmount(parseFloat(inputValue));
 						}}
 						type="number"
 					/>
 					<button
-                        className="p-3 mx-auto block bg-gradient-to-r from-green-400 to-blue-500 hover:bg-gradient-to-l transition duration-1000 ease-in-out rounded-md mt-5 mb-8 w-40 ring-2 ring-blue-500/50"
+						className="p-3 mx-auto block bg-gradient-to-r from-green-400 to-blue-500 hover:bg-gradient-to-l transition duration-1000 ease-in-out rounded-md mt-5 mb-8 w-40 ring-2 ring-blue-500/50"
 						type="button"
 						onClick={convertAmount}
 					>
